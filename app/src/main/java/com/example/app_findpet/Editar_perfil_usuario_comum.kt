32a1500ad1b62f5bb63a7960676693c9d4c5ae92
rafaela <rafaelaandrademoraesz@gmail.com>
2021-10-13 11:26:52 -0300

    telas em andamento

--- app/src/main/java/com/example/app_findpet/Editar_perfil_usuario_comum.kt
+++ app/src/main/java/com/example/app_findpet/Editar_perfil_usuario_comum.kt
@@ -3,9 +3,9 @@ package com.example.app_findpet
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class tela_cadastro_usuarioComum : AppCompatActivity() {
+class Editar_perfil_usuario_comum : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tela_cadastro_usuario_comum)
+        setContentView(R.layout.activity_editar_perfil_usuario_comum)
     }
 }
