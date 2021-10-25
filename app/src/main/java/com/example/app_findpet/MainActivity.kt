bff4e635b3dae4e8669e87aae42e72f3da4fce34
ITALO GABRIEL MESQUITA PEREI <20272538@cfp127aluno.local>
2021-10-25 11:26:22 -0300

    ligando telas

--- app/src/main/java/com/example/app_findpet/MainActivity.kt
+++ app/src/main/java/com/example/app_findpet/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.example.app_findpet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var buttonCadastrar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_inicio)
+
+        supportActionBar!!.hide()
+
+
+        buttonCadastrar = findViewById(R.id.btn_cadastrar)
+
+        buttonCadastrar.setOnClickListener {
+            val intent = Intent(this, tela_escolha_cadastro::class.java)
+            startActivity(intent)
+        }
     }
 }
 
--- app/src/main/java/com/example/app_findpet/tela_escolha_cadastro.kt
+++ app/src/main/java/com/example/app_findpet/tela_escolha_cadastro.kt
@@ -1,11 +1,26 @@
 package com.example.app_findpet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class tela_escolha_cadastro : AppCompatActivity() {
+
+    lateinit var buttonCadastroInstituicao: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_escolha_cadastro)
+
+        supportActionBar!!.hide()
+
+        buttonCadastroInstituicao = findViewById(R.id.btn_cadsatrar_instituicao)
+
+        buttonCadastroInstituicao.setOnClickListener {
+            val intent = Intent(this, tela_escolha_cadastro::class.java)
+            startActivity(intent)
+        }
+
     }
 }
