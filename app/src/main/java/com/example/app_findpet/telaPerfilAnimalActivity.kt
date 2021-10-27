8d53d2b59cf9e7680899b2c4c5ee9c211055b171
rafaela <rafaelaandrademoraesz@gmail.com>
2021-10-27 08:20:58 -0300

    arrumando branch

---
+++ app/src/main/java/com/example/app_findpet/telaPerfilAnimalActivity.kt
@@ -0,0 +1,11 @@
+package com.example.app_findpet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class telaPerfilAnimalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_perfil_animal)
+    }
+}
